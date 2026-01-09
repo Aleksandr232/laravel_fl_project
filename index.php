112a3c527822dacb293d67fae9ffde812b2df956
cz19567 <cz19567@vh444.timeweb.ru>
2026-01-09 13:45:15 +0300

    index

--- index.php
+++ index.php
@@ -1,20 +1,60 @@
 <?php
 
+/**
+ * Laravel - A PHP Framework For Web Artisans
+ *
+ * This file is used as a fallback when DocumentRoot points to the project root
+ * instead of the public directory. It bootstraps the Laravel application.
+ */
+
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+/*
+|--------------------------------------------------------------------------
+| Check If The Application Is Under Maintenance
+|--------------------------------------------------------------------------
+*/
+
 if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
-require __DIR__.'/vendor/autoload.php';
+/*
+|--------------------------------------------------------------------------
+| Register The Auto Loader
+|--------------------------------------------------------------------------
+*/
 
-// Bootstrap Laravel and handle the request...
+if (!file_exists($autoload = __DIR__.'/vendor/autoload.php')) {
+    http_response_code(500);
+    die('Composer autoload file not found. Please run "composer install" on the server.');
+}
+
+require $autoload;
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+*/
+
+$appPath = __DIR__.'/bootstrap/app.php';
+
+if (!file_exists($appPath)) {
+    http_response_code(500);
+    die('Laravel bootstrap file not found.');
+}
+
 /** @var Application $app */
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once $appPath;
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+*/
 
 $app->handleRequest(Request::capture());
